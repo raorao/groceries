4fd40b97c8c9ac36ae67d963e72df6b7237bfe03
raorao <savinirs@gmail.com>
2014-12-27 10:53:56 -0800

    StorageService now encapsulates knowledge of lastTransaction
    lastTransaction check will not be done on front-end. value stored in order
    to facilitate making requests.

--- javascripts/storageService.js
+++ javascripts/storageService.js
@@ -2,14 +2,17 @@
 
 StorageService = (function() {
 
-  var loaded = false
+  var lastTransaction = 0;
 
   var poll = function() {
     // request contains lastTransaction(int) value
     console.log('making request to storage service to check for updates');
     // this will only fire if request returns a 200 status code
-    if(!loaded) { AppAction.load('highestId, lastTransaction, items') };
-    loaded = true
+    if(lastTransaction !== 1) {
+      lastTransaction = 1;
+      AppAction.load('highestId, items')
+    };
+
   };
 
   return {
