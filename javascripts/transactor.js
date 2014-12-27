1c3bf970e6410f9b53fb9518ab13f03dac9afd40
raorao <savinirs@gmail.com>
2014-12-27 10:02:35 -0800

    completes makeRequest skeleton
    TODO: connect to back end

--- javascripts/transactor.js
+++ javascripts/transactor.js
@@ -1,13 +1,21 @@
 Transactor = (function() {
+  var makeRequest = function(payload) {
+    // eventually will include logic to communicate with back end
+    console.log('transactor handles request with payload', payload)
+  }
   return {
     update: function(id, attributes) {
-      console.log('transactor registers update', arguments)
+      var payload = { type: 'update', id: id, attributes: attributes }
+      makeRequest(payload)
     },
     delete: function(id) {
-      console.log('transactor registers deletion', arguments)
+      var payload = { type: 'delete', id: id }
+      makeRequest(payload)
     },
     create: function(item) {
-      console.log('transactor registers creation', arguments)
+      // item has id(int), completed(boolean), and value(text) attributes
+      var payload = { type: 'create', item: item }
+      makeRequest(payload)
     }
   }
 })();
