dfb475e530d1725d2921105e9bb3bf78560a4d37
raorao <savinirs@gmail.com>
2014-12-29 17:58:11 -0800

    generateSnapshot now handles update transactions

--- persistence/dataStore.js
+++ persistence/dataStore.js
@@ -15,20 +15,34 @@ exports.status = function() {
   })
 }
 
-var DEFAULT_CONTENTS = { highestId: 0, items:[] }
+var defaultContents = function() {
+  return { highestId: 0, items:[] }
+}
 
 exports.generateSnapshot = function(transactions, keyList) {
   var contents = keyList.reduce(function(contents,id) {
 
     var transaction = transactions[id];
-    console.log(transactions)
-    // create transaction
-    var item = transaction.item
-    contents.items.push(item);
-    contents.highestId = Math.max(contents.highestId,item.id);
-    return contents
-
-  }, DEFAULT_CONTENTS)
+    switch (transaction.type) {
+      case 'create':
+        var item = transaction.item
+        contents.items.push(item);
+        contents.highestId = Math.max(contents.highestId,item.id);
+        return contents
+        break;
+      case 'update':
+        var attributes = transaction.attributes
+        var items = contents.items.map(function(item) {
+          if (item.id === transaction.id) {
+            for (var key in attributes ) { item[key] = attributes[key]; }
+          }
+          return item
+        })
+        contents.items = items
+        return contents
+        break;
+    }
+  }, defaultContents() )
 
   return contents
 }
