4a745a10e5c6ec31483d24c0e837591e39dbfa70
raorao <savinirs@gmail.com>
2014-12-29 17:17:50 -0800

    adds failing test
    - refactors tests for reusability
    - fixes datatype error in first test

--- persistence/test/dataStoreSpec.js
+++ persistence/test/dataStoreSpec.js
@@ -1,11 +1,25 @@
 var assert = require('assert')
 var dataStore = require('../dataStore')
 
+var CREATE_TRANSACTION = {type: 'create', item: { id: 1, completed: false, value: 'a gallon of milk'} }
+
 describe('generateSnapshot', function() {
+  var transactionKeys, transactions, expected;
+
+  var assertSnapshotEqual = function(expected) {
+    assert.deepEqual(expected, dataStore.generateSnapshot(transactions, transactionKeys))
+  }
+
   it('should return the default value if there are no transactions', function() {
-    var transactionKeys = []
-    var transactions = []
-    var snapshot = dataStore.generateSnapshot(transactions, transactionKeys)
-    assert.deepEqual(snapshot,{ highestId: 0, items: [] })
+    transactionKeys = []
+    transactions = {}
+    assertSnapshotEqual({ highestId: 0, items: [] });
+  })
+
+  it('should handle a transaction of type create', function() {
+    transactionKeys = ['1111']
+    transactions = { '1111': CREATE_TRANSACTION }
+    assertSnapshotEqual({ highestId: 1, items: [CREATE_TRANSACTION.item]})
   })
 })
+
