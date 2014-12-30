c7460871cb085ab16bde6fa52293c8c0610b4cdb
raorao <savinirs@gmail.com>
2014-12-29 17:37:35 -0800

    adds failing test
    abstracts item to own construct for readability

--- persistence/test/dataStoreSpec.js
+++ persistence/test/dataStoreSpec.js
@@ -1,7 +1,9 @@
 var assert = require('assert')
 var dataStore = require('../dataStore')
 
-var CREATE_TRANSACTION = {type: 'create', item: { id: 1, completed: false, value: 'a gallon of milk'} }
+var ITEM = { id: 1, completed: false, value: 'a gallon of milk'}
+var CREATE_TRANSACTION = {type: 'create', item: ITEM }
+var UPDATE_TRANSACTION = {type: 'update', id: 1, attributes: { value: 'new value' } }
 
 describe('generateSnapshot', function() {
   var transactionKeys, transactions, expected;
@@ -20,6 +22,13 @@ describe('generateSnapshot', function() {
     transactionKeys = ['1111']
     transactions = { '1111': CREATE_TRANSACTION }
     assertSnapshotEqual({ highestId: 1, items: [CREATE_TRANSACTION.item]})
+  })
+
+  it('should handle a transaction of type update', function() {
+    transactionKeys = ['1111', '2222']
+    transactions = { '1111': CREATE_TRANSACTION, '2222': UPDATE_TRANSACTION }
+    updatedItem = { id: ITEM.id, completed: ITEM.completed, value: UPDATE_TRANSACTION.attributes.value }
+    assertSnapshotEqual({ highestId: 1, items: [updatedItem]})
   })
 })
 
