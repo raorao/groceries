2e735e40af76c3bea0a3c595c4f1e1e72cc67f6d
raorao <savinirs@gmail.com>
2014-12-29 16:51:23 -0800

    adds skeleton testing suite

---
+++ persistence/test/dataStoreSpec.js
@@ -0,0 +1,12 @@
+var assert = require('assert')
+
+
+describe('example test', function() {
+  it('should pass', function() {
+    assert.equal(true, true)
+  });
+
+  it('should fail', function() {
+    assert.equal(true,false)
+  })
+})
