67bbea7ffdce1454de0f960777f6cc85bf6c3278
raorao <savinirs@gmail.com>
2014-12-31 15:51:53 -0800

    abstracts transactor calls to redis

--- persistence/db/transactor.js
+++ persistence/db/transactor.js
@@ -1,9 +1,18 @@
 var db = require('./client')
 var client = db.client
-var print = db.print
 
-exports.add = function(payload) {
+var addTransaction = function(key, payload) {
+  client.hset('transactions', key, payload, db.print)
+}
+
+var addTransactionKey = function(payload) {
+  client.rpush('transactionKeys', payload, db.print)
+}
+
+
+
+exports.add = function(transaction) {
   var key = new Date().getTime();
-  client.hset('transactions', key, payload, print)
-  client.rpush('transactionKeys', key, print)
+  addTransaction(key,transaction);
+  addTransactionKey(key);
 }
